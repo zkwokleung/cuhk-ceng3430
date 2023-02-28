d84b3ccebe6aa8ae567a72e93a4d7f7f690aa108
Andrew SZETO <zkwokleung@gmail.com>
2023-02-28 16:35:55 +0800

    Update lab06

--- Lab06/lab06.vhd
+++ Lab06/lab06.vhd
@@ -1,72 +1,91 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
-entity lab06 is
-  Port (
-    -- TODO-1: Fill in the input/output ports for Pmod SSD and switches
-    
-    -- TODO-10: Fill in the input/output ports for Pmod ALS
+ENTITY lab06 IS
+  PORT (
+    -- Pmod SSD and switches
+    switch : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    clk : IN STD_LOGIC;
+    ssd : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+    sel : BUFFER STD_LOGIC;
+
+    -- Pmod ALS
+    cs : BUFFER STD_LOGIC;
+    sclk : BUFFER STD_LOGIC;
+    miso : IN STD_LOGIC
   );
-end lab06;
+END lab06;
 
-architecture Behavioral of lab06 is
+ARCHITECTURE Behavioral OF lab06 IS
 
--- TODO-7: create the component of sst_ctrl
+  -- create the component of sst_ctrl
+  COMPONENT ssd_ctrl
+    PORT (
+      clk : IN STD_LOGIC; --system clock
+      data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0); --data to display
+      sel : BUFFER STD_LOGIC;
+      ssd : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
+    );
+  END COMPONENT;
 
---  TODO-11: Uncomment the component
---component spi_master
---  generic(
---    slaves  : INTEGER;  --number of spi slaves
---    d_width : INTEGER); --data bus width
---  port(
---    clock   : IN     STD_LOGIC;                             --system clock
---    reset_n : IN     STD_LOGIC;                             --asynchronous reset
---    enable  : IN     STD_LOGIC;                             --initiate transaction
---    cpol    : IN     STD_LOGIC;                             --spi clock polarity
---    cpha    : IN     STD_LOGIC;                             --spi clock phase
---    cont    : IN     STD_LOGIC;                             --continuous mode command
---    clk_div : IN     INTEGER;                               --system clock cycles per 1/2 period of sclk
---    addr    : IN     INTEGER;                               --address of slave
---    tx_data : IN     STD_LOGIC_VECTOR(d_width-1 DOWNTO 0);  --data to transmit
---    miso    : IN     STD_LOGIC;                             --master in, slave out
---    sclk    : BUFFER STD_LOGIC;                             --spi clock
---    ss_n    : BUFFER STD_LOGIC_VECTOR(slaves-1 DOWNTO 0);   --slave select
---    mosi    : OUT    STD_LOGIC;                             --master out, slave in
---    busy    : OUT    STD_LOGIC;                             --busy / data ready signal
---    rx_data : OUT    STD_LOGIC_VECTOR(d_width-1 DOWNTO 0) --data received
---    );
---end component;
+  COMPONENT spi_master
+    GENERIC (
+      slaves : INTEGER; --number of spi slaves
+      d_width : INTEGER); --data bus width
+    PORT (
+      clock : IN STD_LOGIC; --system clock
+      reset_n : IN STD_LOGIC; --asynchronous reset
+      enable : IN STD_LOGIC; --initiate transaction
+      cpol : IN STD_LOGIC; --spi clock polarity
+      cpha : IN STD_LOGIC; --spi clock phase
+      cont : IN STD_LOGIC; --continuous mode command
+      clk_div : IN INTEGER; --system clock cycles per 1/2 period of sclk
+      addr : IN INTEGER; --address of slave
+      tx_data : IN STD_LOGIC_VECTOR(d_width - 1 DOWNTO 0); --data to transmit
+      miso : IN STD_LOGIC; --master in, slave out
+      sclk : BUFFER STD_LOGIC; --spi clock
+      ss_n : BUFFER STD_LOGIC_VECTOR(slaves - 1 DOWNTO 0); --slave select
+      mosi : OUT STD_LOGIC; --master out, slave in
+      busy : OUT STD_LOGIC; --busy / data ready signal
+      rx_data : OUT STD_LOGIC_VECTOR(d_width - 1 DOWNTO 0) --data received
+    );
+  END COMPONENT;
 
--- TODO-13: Create a signal for rx_data
+  -- signal for rx_data
+  SIGNAL rx_data : STD_LOGIC_VECTOR(14 DOWNTO 0);
 
-begin
+BEGIN
 
--- TODO-8: Port map the ssd_ctrl
--- TODO-14: Send the rx_data from spi_master to ssd_ct 
+  -- Port map the ssd_ctrl
+  ssd_ctrl_0 : ssd_ctrl
+  PORT MAP(
+    clk => clk,
+    data_in => rx_data(11 DOWNTO 4),
+    sel => sel,
+    ssd => ssd
+  );
+  -- TODO-14: Send the rx_data from spi_master to ssd_ct 
+  spi_master_0 : spi_master
+  GENERIC MAP(
+    slaves => 1,
+    d_width => 15
+  )
+  PORT MAP(
+    clock => clk,
+    clk_div => 50000,
+    sclk => sclk,
+    miso => miso,
+    ss_n(0) => cs,
+    mosi => OPEN,
+    reset_n => '1',
+    addr => 0,
+    cpol => '1',
+    cpha => '1',
+    tx_data => (OTHERS => '0'),
+    rx_data => rx_data,
+    enable => '1',
+    busy => OPEN,
+    cont => '0'
+  );
 
-
---  TODO-12: Uncomment the component
---    spi_master_0: spi_master
---    generic map(
---        slaves => ,
---        d_width => 
---    )
---    port map(
---        clock => ,
---        clk_div => ,
---        sclk => ,
---        miso => ,
---        ss_n => ,
---        mosi => ,
---        reset_n => ,
---        addr => ,
---        cpol => ,
---        cpha => ,
---        tx_data => ,
---        rx_data => ,
---        enable => ,
---        busy => ,
---        cont => 
---    );
-
-end Behavioral;
+END Behavioral;
--- Lab06/ssd_ctrl.vhd
+++ Lab06/ssd_ctrl.vhd
@@ -3,18 +3,28 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY ssd_ctrl IS
   PORT (
-    -- TODO-1: Create the input/output ports
+    clk : IN STD_LOGIC;
+    data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    sel : BUFFER STD_LOGIC := '0';
+    ssd : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
   );
 END ssd_ctrl;
 
 ARCHITECTURE Behavioral OF ssd_ctrl IS
-  -- TODO-4: Create the component of clk_div
+  COMPONENT clock_divider
+    GENERIC (
+      N : INTEGER
+    );
+    PORT (
+      CLK_IN : IN STD_LOGIC;
+      CLK_OUT : OUT STD_LOGIC
+    );
+  END COMPONENT;
 
-  -- Add any signal if necessary
   SIGNAL digit : STD_LOGIC_VECTOR(3 DOWNTO 0);
+  SIGNAL clk100Hz : STD_LOGIC;
 
 BEGIN
-  -- TODO-2: Fill in the blank
   PROCESS (digit) BEGIN
     CASE digit IS
       WHEN "0000" => ssd <= "1111110"; -- 0x0
@@ -37,9 +47,40 @@ BEGIN
     END CASE;
   END PROCESS;
 
-  -- TODO-5 : Port map the clk_div component (100MHz --> 100Hz)
+  -- Port map the clk_div component (100MHz --> 100Hz)
+  clk_div_inst : clock_divider
+  GENERIC MAP(
+    N => 50000
+  )
+  PORT MAP(
+    CLK_IN => clk,
+    CLK_OUT => clk100Hz
+  );
 
-  -- TODO-6: Time-multiplexing (Create as many process as you want, OR use both sequential and combinational statement)
-  digit <= data_in(3 DOWNTO 0);
+  -- Toggle the selector
+  PROCESS (clk100Hz) BEGIN
+    IF (rising_edge(clk100Hz)) THEN
+      IF (sel = '0') THEN
+        sel <= '1';
+      ELSIF (sel = '1') THEN
+        sel <= '0';
+      ELSE
+        sel <= '0';
+      END IF;
+    END IF;
+  END PROCESS;
+
+  -- Control the digit displayed
+  PROCESS (clk100Hz) BEGIN
+    IF (rising_edge(clk100Hz)) THEN
+      IF (sel = '0') THEN
+        digit <= data_in(7 DOWNTO 4);
+      ELSIF (sel = '1') THEN
+        digit <= data_in(3 DOWNTO 0);
+      ELSE
+        digit <= digit;
+      END IF;
+    END IF;
+  END PROCESS;
 
 END Behavioral;
