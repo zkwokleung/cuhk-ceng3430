b3b5484fe35c44c01af39a56de3a8cec691df094
Andrew SZETO <zkwokleung@gmail.com>
2023-03-14 18:42:04 +0800

    Update Lab07

--- Lab07/stopwatch.c
+++ Lab07/stopwatch.c
@@ -1,6 +1,6 @@
 #include "stopwatch_controller.h"
-#include "xparameters.h"
 #include "xil_io.h"
+#include "xparameters.h"
 #include <unistd.h>
 
 /* Definitions Section */
@@ -28,69 +28,103 @@
 #define SW_1 2
 #define SW_0 1
 
-int main(void){
+#define S_STOPPED 0
+#define S_COUNTDOWN 1
+#define S_COUNTUP 2
 
-	/* INPUT */
-	u32 btn_in = 0;
-	u32 switch_in = 0;
-	u32 timer_in = 0;
+int main(void)
+{
 
-	/* INTERNAL */
-	u32 stopped = 1;
-	u32 btn_in_prev = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, BTN_ADDR);
-	u32 switch_in_prev = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, SWITCH_ADDR);
-	u32 timer_zero = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, TIMER_ADDR);
+    /* INPUT */
+    u32 btn_in = 0;
+    u32 switch_in = 0;
+    u32 timer_in = 0;
 
-	/* OUTPUT */
-	u32 ssd_out = 0;
-	u32 led_out = 0;
+    /* INTERNAL */
+    u32 btn_in_prev = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, BTN_ADDR);
+    u32 switch_in_prev = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, SWITCH_ADDR);
+    u32 timer_zero = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, TIMER_ADDR);
 
-	/* User defined variable */
+    /* OUTPUT */
+    u32 ssd_out = 0;
+    u32 led_out = 0;
 
-	while(1)
-	{
-		/*** INPUT ***/
+    /* User defined variable */
+    u32 state = S_STOPPED;
 
-		/* btn */
-		btn_in = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, BTN_ADDR);
-		u32 btn_rise = ~btn_in_prev & btn_in;
-		if (btn_rise & BTN_C) stopped=(stopped==1?0:1);
+    while (1)
+    {
+        /*** INPUT ***/
 
-		/* switch */
-		switch_in = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, SWITCH_ADDR);
-		if (switch_in != switch_in_prev) stopped = 1;
+        /* btn */
+        btn_in = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, BTN_ADDR);
+        u32 btn_rise = ~btn_in_prev & btn_in;
 
-		/* time */
-		timer_in = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, TIMER_ADDR);
-		int time_display;
-		if(stopped)
-		{
-			time_display = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, SWITCH_ADDR);
-			timer_zero = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, TIMER_ADDR);
-		}else
-		{
-			u32 time_elapsed = (timer_in - timer_zero) / 1000; // seconds
-			time_display = switch_in - time_elapsed;
-			if(time_display < 0)
-			{
-				timer_zero = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, TIMER_ADDR);
-			}
-		}
+        /* switch */
+        switch_in = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, SWITCH_ADDR);
 
-		/*** OUTPUT ***/
+        /* time */
+        timer_in = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, TIMER_ADDR);
+        int time_display;
+        u32 time_elapsed;
+        switch (state)
+        {
+        case S_STOPPED:
+            time_display = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, SWITCH_ADDR);
+            timer_zero = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, TIMER_ADDR);
 
-		/* led */
-		led_out = time_display;
-		STOPWATCH_CONTROLLER_mWriteReg(SW_BASE, LED_ADDR, led_out);
+            if (btn_rise & BTN_D)
+                state = S_COUNTDOWN;
+            else if(btn_rise & BTN_U)
+            	state = S_COUNTUP;
 
-		/* ssd */
-		ssd_out = time_display;
-		STOPWATCH_CONTROLLER_mWriteReg(SW_BASE, SSD_ADDR, ssd_out);
+            break;
 
-		/*** FEEDBACK ***/
-		
-		btn_in_prev = btn_in;
-		switch_in_prev = switch_in;
-	}
-	return 1;
+        case S_COUNTDOWN:
+        	time_elapsed = (timer_in - timer_zero) / 500; // seconds
+            time_display = switch_in - time_elapsed;
+            if (time_display < 0)
+            {
+                timer_zero = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, TIMER_ADDR);
+            }
+
+            if (btn_rise & BTN_C)
+            	state = S_STOPPED;
+
+            break;
+
+        case S_COUNTUP:
+            time_elapsed = (timer_in - timer_zero) / 1000; // seconds
+            time_display = switch_in + time_elapsed;
+            if (time_display > 255)
+            {
+                timer_zero = STOPWATCH_CONTROLLER_mReadReg(SW_BASE, TIMER_ADDR);
+            }
+
+
+            if (btn_rise & BTN_C)
+            	state = S_STOPPED;
+
+            break;
+
+        default:
+            break;
+        }
+
+        /*** OUTPUT ***/
+
+        /* led */
+        led_out = time_display;
+        STOPWATCH_CONTROLLER_mWriteReg(SW_BASE, LED_ADDR, led_out);
+
+        /* ssd */
+        ssd_out = time_display;
+        STOPWATCH_CONTROLLER_mWriteReg(SW_BASE, SSD_ADDR, ssd_out);
+
+        /*** FEEDBACK ***/
+
+        btn_in_prev = btn_in;
+        switch_in_prev = switch_in;
+    }
+    return 1;
 }
