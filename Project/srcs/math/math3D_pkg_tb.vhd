3dd979cd31e36584404642160964999186972e8b
Andrew SZETO <zkwokleung@gmail.com>
2023-05-04 01:51:31 +0800

    Update math3D_pkg_tb

--- Project/srcs/math/math3D_pkg_tb.vhd
+++ Project/srcs/math/math3D_pkg_tb.vhd
@@ -7,16 +7,28 @@ ENTITY math3D_pkg_tb IS
 END math3D_pkg_tb;
 
 ARCHITECTURE math3D_pkg_tb_arch OF math3D_pkg_tb IS
-    SIGNAL a, b, c, d, e, f : vec3_int;
-    SIGNAL i, j, k, l, m, n : vec4_int;
-    SIGNAL x, y, z, w : INTEGER;
+    SIGNAL a : vec3_int := (1, 2, 3);
+    SIGNAL b : vec3_int := (2, 3, 4);
+    SIGNAL c : vec3_int := (-1, -1, -1);
+
+    SIGNAL x : mat4_int := ((1, 2, 3, 4),
+    (5, 6, 7, 8),
+    (9, 10, 11, 12),
+    (13, 14, 15, 16));
+
+    SIGNAL y : mat4_int := ((1, 2, 3, 4),
+    (5, 6, 7, 8),
+    (9, 10, 11, 12),
+    (13, 14, 15, 16));
+
+    SIGNAL z : mat4_int := ((0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0));
 BEGIN
 
     PROCESS
     BEGIN
-        a <= (1, 2, 3);
-        b <= (4, 5, 6);
+        WAIT FOR 10 ns;
         c <= a + b;
+        z <= x * y;
         WAIT FOR 10 ns;
 
         WAIT;
