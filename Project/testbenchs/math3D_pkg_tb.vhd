8ac8c906bdd3e794b3626c4cb18c880533513c51
Andrew SZETO <56270363+zkwokleung@users.noreply.github.com>
2023-05-11 21:09:46 +0800

    Update math3D tb

--- Project/testbenchs/math3D_pkg_tb.vhd
+++ Project/testbenchs/math3D_pkg_tb.vhd
@@ -7,8 +7,8 @@ ENTITY math3D_pkg_tb IS
 END math3D_pkg_tb;
 
 ARCHITECTURE math3D_pkg_tb_arch OF math3D_pkg_tb IS
-    SIGNAL a : vec3_int := (1, 2, 3);
-    SIGNAL b : vec3_int := (2, 3, 4);
+    SIGNAL a : vec3_int := (600, 300, 3);
+    SIGNAL b : vec3_int := (0, 0, 0);
     SIGNAL c : vec3_int := (-1, -1, -1);
 
     SIGNAL x : mat4_int := (
@@ -33,31 +33,41 @@ ARCHITECTURE math3D_pkg_tb_arch OF math3D_pkg_tb IS
     SIGNAL ipt_float : mat4_float;
     SIGNAL o : mat4_float;
     SIGNAL o_int : mat4_int;
+
+    SIGNAL v : vec3_float;
+    SIGNAL v4f : vec4_float;
 BEGIN
 
     PROCESS
     BEGIN
-        pp <= perspective_128_75_mat4_float;
-        WAIT FOR 10 ns;
-        ipt_float <= to_mat4_float(x);
-        WAIT FOR 10 ns;
-        o <= pp * ipt_float;
-        WAIT FOR 10 ns;
-        o_int <= to_mat4_int(o);
-        WAIT FOR 10 ns;
+        -- pp <= perspective_128_75_mat4_float;
+        -- WAIT FOR 10 ns;
+        -- ipt_float <= to_mat4_float(x);
+        -- WAIT FOR 10 ns;
+        -- o <= pp * ipt_float;
+        -- WAIT FOR 10 ns;
+        -- o_int <= to_mat4_int(o);
+        -- WAIT FOR 10 ns;
 
-        ipt_float <= to_mat4_float(y);
-        WAIT FOR 10 ns;
-        o <= pp * ipt_float;
-        WAIT FOR 10 ns;
-        o_int <= to_mat4_int(o);
-        WAIT FOR 10 ns;
+        -- ipt_float <= to_mat4_float(y);
+        -- WAIT FOR 10 ns;
+        -- o <= pp * ipt_float;
+        -- WAIT FOR 10 ns;
+        -- o_int <= to_mat4_int(o);
+        -- WAIT FOR 10 ns;
 
-        ipt_float <= to_mat4_float(z);
+        -- ipt_float <= to_mat4_float(z);
+        -- WAIT FOR 10 ns;
+        -- o <= pp * ipt_float;
+        -- WAIT FOR 10 ns;
+        -- o_int <= to_mat4_int(o);
+        -- WAIT FOR 10 ns;
+
+        v4f <= to_vec4_float(to_vec3_float(a), float32_one);
         WAIT FOR 10 ns;
-        o <= pp * ipt_float;
+        v4f <= pp * v4f;
         WAIT FOR 10 ns;
-        o_int <= to_mat4_int(o);
+        v <= (v4f(0), v4f(1), v4f(2)) / v4f(3);
         WAIT FOR 10 ns;
 
         WAIT;
