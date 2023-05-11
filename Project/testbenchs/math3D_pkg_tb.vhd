d2b40238ab8b26c0f1353779eead9c254032ec2a
Andrew SZETO <56270363+zkwokleung@users.noreply.github.com>
2023-05-11 20:38:55 +0800

    Update math3d pkg tb

--- Project/testbenchs/math3D_pkg_tb.vhd
+++ Project/testbenchs/math3D_pkg_tb.vhd
@@ -11,24 +11,32 @@ ARCHITECTURE math3D_pkg_tb_arch OF math3D_pkg_tb IS
     SIGNAL b : vec3_int := (2, 3, 4);
     SIGNAL c : vec3_int := (-1, -1, -1);
 
-    SIGNAL x : mat4_int := ((1, 2, 3, 4),
-    (5, 6, 7, 8),
-    (9, 10, 11, 12),
-    (13, 14, 15, 16));
+    SIGNAL x : mat4_int := (
+    (1, 2, 3, 4),
+        (5, 6, 7, 8),
+        (9, 10, 11, 12),
+        (13, 14, 15, 16)
+    );
 
     SIGNAL y : mat4_int := ((1, 2, 3, 4),
     (5, 6, 7, 8),
     (9, 10, 11, 12),
     (13, 14, 15, 16));
 
-    SIGNAL z : mat4_int := ((0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0));
+    SIGNAL z : mat4_int := ((0, 0, 0, 0),
+    (0, 0, 0, 0),
+    (0, 0, 0, 0),
+    (0, 0, 0, 0));
+
+    SIGNAL pp : mat4_float;
+    SIGNAL pp_int : mat4_int;
 BEGIN
 
     PROCESS
     BEGIN
+        pp <= perspective_128_75_mat4_float;
         WAIT FOR 10 ns;
-        c <= a + b;
-        z <= x * y;
+        pp_int <= to_mat4_int(pp);
         WAIT FOR 10 ns;
 
         WAIT;
