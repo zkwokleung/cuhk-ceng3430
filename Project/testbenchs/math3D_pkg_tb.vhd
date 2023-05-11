fc61f64b3a6b01026868ad73ce50b2c4af8ae677
Andrew SZETO <zkwokleung@gmail.com>
2023-05-11 20:54:27 +0800

    Update math3d pkg testbench

--- Project/testbenchs/math3D_pkg_tb.vhd
+++ Project/testbenchs/math3D_pkg_tb.vhd
@@ -12,31 +12,52 @@ ARCHITECTURE math3D_pkg_tb_arch OF math3D_pkg_tb IS
     SIGNAL c : vec3_int := (-1, -1, -1);
 
     SIGNAL x : mat4_int := (
-    (1, 2, 3, 4),
-        (5, 6, 7, 8),
-        (9, 10, 11, 12),
-        (13, 14, 15, 16)
+    (100, 200, 300, 400),
+        (500, 600, 700, 8),
+        (900, 1000, 1100, 12),
+        (1300, 1400, 1500, 16)
     );
 
-    SIGNAL y : mat4_int := ((1, 2, 3, 4),
+    SIGNAL y : mat4_int := ((100, 2, 3, 4),
     (5, 6, 7, 8),
     (9, 10, 11, 12),
     (13, 14, 15, 16));
 
-    SIGNAL z : mat4_int := ((0, 0, 0, 0),
-    (0, 0, 0, 0),
-    (0, 0, 0, 0),
-    (0, 0, 0, 0));
+    SIGNAL z : mat4_int := (
+    (110, 110, 110, 110),
+        (300, 300, 300, 300),
+        (300, 300, 300, 300),
+        (300, 300, 300, 300));
 
     SIGNAL pp : mat4_float;
-    SIGNAL pp_int : mat4_int;
+    SIGNAL ipt_float : mat4_float;
+    SIGNAL o : mat4_float;
+    SIGNAL o_int : mat4_int;
 BEGIN
 
     PROCESS
     BEGIN
         pp <= perspective_128_75_mat4_float;
         WAIT FOR 10 ns;
-        pp_int <= to_mat4_int(pp);
+        ipt_float <= to_mat4_float(x);
+        WAIT FOR 10 ns;
+        o <= pp * ipt_float;
+        WAIT FOR 10 ns;
+        o_int <= to_mat4_int(o);
+        WAIT FOR 10 ns;
+
+        ipt_float <= to_mat4_float(y);
+        WAIT FOR 10 ns;
+        o <= pp * ipt_float;
+        WAIT FOR 10 ns;
+        o_int <= to_mat4_int(o);
+        WAIT FOR 10 ns;
+
+        ipt_float <= to_mat4_float(z);
+        WAIT FOR 10 ns;
+        o <= pp * ipt_float;
+        WAIT FOR 10 ns;
+        o_int <= to_mat4_int(o);
         WAIT FOR 10 ns;
 
         WAIT;
