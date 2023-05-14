a3b3ce92fbf36dea9c0cd5739bc96bdb0ed169ad
Andrew SZETO <zkwokleung@gmail.com>
2023-05-14 23:41:15 +0800

    Fix VGA clock

--- Project/srcs/vga/vga_controller.vhd
+++ Project/srcs/vga/vga_controller.vhd
@@ -23,6 +23,14 @@ ENTITY vga_controller IS
 END vga_controller;
 
 ARCHITECTURE vga_controller_arch OF vga_controller IS
+    COMPONENT clock_divider IS
+        GENERIC (N : INTEGER);
+        PORT (
+            CLK_IN : IN STD_LOGIC;
+            CLK_OUT : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
     --------- VGA CONSTANT START ---------
     -- row constants
     CONSTANT h_total : INTEGER := 1344 - 1;
@@ -43,14 +51,22 @@ ARCHITECTURE vga_controller_arch OF vga_controller IS
     CONSTANT v_front : INTEGER := 10 - 1;
 
     SIGNAL h_count, v_count : INTEGER;
+    SIGNAL clk_50Mhz : STD_LOGIC;
     --------- VGA CONSTANT END ---------    
 BEGIN
+    -- Clock divider
+    clk_divider : clock_divider
+    GENERIC MAP(N => 1)
+    PORT MAP(
+        CLK_IN => CLK,
+        CLK_OUT => clk_50Mhz
+    );
+
     --------- VGA UTILITY START ---------
     -- horizontal counter in [0, h_total]
-    pixel_count_proc : PROCESS (CLK)
+    pixel_count_proc : PROCESS (clk_50Mhz)
     BEGIN
-        IF (rising_edge(CLK))
-            THEN
+        IF (rising_edge(clk_50Mhz)) THEN
             IF (h_count = h_total)
                 THEN
                 h_count <= 0;
@@ -61,27 +77,22 @@ BEGIN
     END PROCESS pixel_count_proc;
 
     -- generate hsync in [0, h_sync)
-    hsync_gen_proc : PROCESS (CLK, h_count)
+    hsync_gen_proc : PROCESS (h_count)
     BEGIN
-        IF (rising_edge(CLK)) THEN
-            IF (h_count <= h_sync)
-                THEN
-                VGA_HSYNC <= '1';
-            ELSE
-                VGA_HSYNC <= '0';
-            END IF;
+        IF (h_count <= h_sync)
+            THEN
+            VGA_HSYNC <= '1';
+        ELSE
+            VGA_HSYNC <= '0';
         END IF;
     END PROCESS hsync_gen_proc;
 
     -- vertical counter in [0, v_total]
-    line_count_proc : PROCESS (CLK)
+    line_count_proc : PROCESS (clk_50Mhz)
     BEGIN
-        IF (rising_edge(CLK))
-            THEN
-            IF (h_count = h_total)
-                THEN
-                IF (v_count = v_total)
-                    THEN
+        IF (rising_edge(clk_50Mhz)) THEN
+            IF (h_count = h_total) THEN
+                IF (v_count = v_total) THEN
                     v_count <= 0;
                 ELSE
                     v_count <= v_count + 1;
@@ -91,45 +102,43 @@ BEGIN
     END PROCESS line_count_proc;
 
     -- generate vsync in [0, v_sync)
-    vsync_gen_proc : PROCESS (CLK, v_count)
+    vsync_gen_proc : PROCESS (v_count)
     BEGIN
-        IF (rising_edge(CLK)) THEN
-            IF (v_count <= v_sync) THEN
-                VGA_VSYNC <= '1';
-            ELSE
-                VGA_VSYNC <= '0';
-            END IF;
+        IF (v_count <= v_sync) THEN
+            VGA_VSYNC <= '1';
+        ELSE
+            VGA_VSYNC <= '0';
         END IF;
     END PROCESS vsync_gen_proc;
     --------- VGA UTILITY END ---------
 
-    -- Rendering process
-    PROCESS (CLK, h_count, v_count, RED_IN, GREEN_IN, BLUE_IN)
+    -- Calculate the coordinates
+    coor_calc_proc : PROCESS (clk_50Mhz)
     BEGIN
-        IF (rising_edge(CLK)) THEN
+        IF (rising_edge(clk_50Mhz)) THEN
+
+            -- Calculate the coordinates
+            COOR_H <= h_count - h_start;
+            COOR_V <= v_count - v_start;
+            NEXT_COOR_H <= h_count - h_start + 1;
+            NEXT_COOR_V <= v_count - v_start + 1;
+        END IF;
+    END PROCESS;
+
+    -- Rendering process
+    render_proc : PROCESS (clk_50Mhz)
+    BEGIN
+        IF (rising_edge(clk_50Mhz)) THEN
             -- Check if we are in the active area
             IF ((h_count >= h_start AND h_count <= h_end) AND
                 (v_count >= v_start AND v_count <= v_end))
                 THEN
-                -- Calculate the coordinates
-                COOR_H <= h_count - h_start;
-                COOR_V <= v_count - v_start;
-                NEXT_COOR_H <= h_count - h_start + 1;
-                NEXT_COOR_V <= v_count - v_start + 1;
-
                 -- Render the color
                 VGA_RED <= RED_IN;
                 VGA_GREEN <= GREEN_IN;
                 VGA_BLUE <= BLUE_IN;
 
             ELSE
-                -- Set the coordinates to 0
-                COOR_H <= 0;
-                COOR_V <= 0;
-
-                NEXT_COOR_H <= 0;
-                NEXT_COOR_V <= 0;
-
                 -- Set the color to black
                 VGA_RED <= "0000";
                 VGA_GREEN <= "0000";
